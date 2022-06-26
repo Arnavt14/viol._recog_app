9a1e2b0299999bc8eb05e0b86970cd7c887c9356
Arnavt14 <77206185+Arnavt14@users.noreply.github.com>
2022-06-27 04:23:00 +0800

    Firebase config

---
+++ main.py
@@ -0,0 +1,19 @@
+# modules/lib
+import pyrebase
+import streamlit as st
+
+
+# Firebase configuration key
+firebaseConfig = {
+  "apiKey": "AIzaSyANvY1WusKuNfpCW3WYagFSd9mPIBtUN_0",
+  "authDomain": "stay-safe-c7497.firebaseapp.com",
+  "databaseURL": "https://stay-safe-c7497-default-rtdb.firebaseio.com",
+  "projectId": "stay-safe-c7497",
+  "storageBucket": "stay-safe-c7497.appspot.com",
+  "messagingSenderId": "510281232102",
+  "appId": "1:510281232102:web:82d6246255cbca22f0d527",
+  "measurementId": "G-1FQC62M1V7"
+};
+
+
+# Firebase authentication
